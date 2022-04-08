279783b78d1e53935430de3901d46d3479b3f1ef
Syaza_Ismail <Syaza_Ismail@W108D4N2X2>
2022-04-08 12:23:34 +0800

    Add project files.

---
+++ DSA/Algorithm/LinearSearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Algorithm
+{
+    class LinearSearch
+    {
+        int[] array = { 59, 23, 61, 90, 33, 2, 30 };
+    }
+    // T(n) => O(n)
+    public static void Do()
+    {
+        Console.WriteLine("Please enter a number to search in array");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < Array.Length; i++)
+        {
+            if (Array[i] == num)
+            {
+                Console.WriteLine("Number found at index " = i);
+                return;
+            }
+        }
+        Console.WriteLine("Number not found in array");
+    }
+}
---
+++ DSA/LinkedList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    class LinkedList
+    {
+        Node head;
+        Node firstNode;
+    
+        public void Do()
+        {
+            var fn = firstNode = head = new Node(2);
+            var node3 = Insert(head, 3);
+            var node4 = Insert(head, 5);
+            var node5 = Insert(head, 6);
+            this.Print();
+            firstNode = fn;
+            this.Delete(node5, node4);
+            this.Print();
+            firstNode = fn;
+            var node6 = Insert(head, 6);
+            var node7 = Insert(head, 7);
+            this.Print();
+
+        }
+
+        Node Insert(Node head, int num)
+        {
+            Node newNode = new Node(num);
+            head.next = newNode;
+            this.head = newNode;
+            return newNode;
+        }
+
+        //3=>4=>5=>null
+        //3=>
+        void Delete(Node nodeToDelete, Node previousNode)
+        {
+            previousNode.next = nodeToDelete.next;
+            this.head = previousNode;
+        }
+
+        void Print()
+        {
+            while(this.firstNode != null)
+            {
+                Console.WriteLine(firstNode.value);
+                firstNode = firstNode.next;
+            }
+            Console.WriteLine("");
+        }
+    }
+
+
+    class Node
+    {
+        public int value;
+        public Node next;
+
+        public Node(int val)
+        {
+            value = val;
+            next = null;
+        }
+    }
+}
